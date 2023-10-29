41dad7169f9727df113cf37b84ae5010ab4a4e7a
TarcaAndreea <adrianatarca1@gmail.com>
2023-10-29 20:23:41 +0200

    hw02

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,100 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../DB/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    code: 200,
+    data: "Server response ok",
+  });
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  try {
+    const contact = await getContactById(contactId);
+    res.status(200).json({
+      status: "success",
+      code: 200,
+      data: { ...contact },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "error",
+      code: 404,
+      message: "Contact not found",
+    });
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const { name, email, phone } = req.body;
+  try {
+    const data = await addContact({ name, email, phone });
+    res.status(201).json({
+      status: "success",
+      code: 201,
+      data: data,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      code: 400,
+      message: "Missing required name field",
+    });
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  try {
+    await removeContact(contactId);
+    res.status(200).json({
+      status: "success",
+      code: 200,
+      message: "Contact deleted",
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "error",
+      code: 404,
+      message: "Not found",
+    });
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400).json({
+      status: "error",
+      code: 400,
+      message: "Missing required field",
+    });
+  }
+  try {
+    const data = await updateContact(contactId, { name, email, phone });
+    res.status(200).json({
+      status: "success",
+      code: 200,
+      data: data,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "error",
+      code: 404,
+      message: "Not found",
+    });
+  }
+});
 
-module.exports = router
+module.exports = router;
